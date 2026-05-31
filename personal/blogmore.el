9579e7e80a45b8c969c3428cc1a4155d2eb27b2e
Chris Jobling <cpjobling@cpjobling.net>
2026-05-31 13:21:02 +0200

    Added blogmore package

---
+++ personal/blogmore.el
@@ -0,0 +1,33 @@
+;;; -*- lexical-binding: t; -*-
+
+(use-package blogmore
+  :ensure t
+  :defer t
+  :vc (:url "https://github.com/davep/blogmore.el" :rev :newest)
+  :init
+  ;; Add an end-of-file marker to any new post.
+  (add-hook 'blogmore-new-post-hook #'end-it)
+  ;; Always start out working on my personal blog.
+  (blogmore-work-on "blog.cpjobling.net")
+  ;; Add some useful abbrevs for inserting commonly-used links into my blog
+  ;; posts.
+  (define-abbrev-table 'markdown-mode-abbrev-table
+    '(("bm" "[BlogMore](https://blogmore.davep.dev/)")
+      ("bme" "[`blogmore.el`](https://github.com/davep/blogmore.el)")
+      ("pblog" "[photoblog](https://seen-by.davep.dev/)")))
+  :custom
+  (blogmore-blogs
+   (list
+    (blogmore-blog
+     :title "blog.cpjobling.net"
+     :posts-directory "~/code/src/github.com/cpjobling/blog.cpjobling.me"
+     :post-subdirectory-function (lambda () (format-time-string "%Y/%m/")))
+    (blogmore-blog
+     :title "seen-by.davep.dev"
+     :posts-directory "~/write/seen-by/content/posts/"))
+   )
+  :bind
+  ("<f12> b" . blogmore)
+  )
+
+;;; blogmore.el ends here
